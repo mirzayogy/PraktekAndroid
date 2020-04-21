a6f717fa9e0a8e44d861517d42cc6c2cd92cee05
mirzayogy <mirza.yogy@gmail.com>
2020-04-22 03:44:58 +0800

    Penambahan HitungActivity, panggil dengan intent

---
+++ app/src/main/java/com/praktekandroid/HitungActivity.kt
@@ -0,0 +1,12 @@
+package com.praktekandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HitungActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hitung)
+    }
+}
